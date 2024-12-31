2a4a8894a895c1014494d2d44e237552c75bcce0
sanskar sawant <sanskarsawant23@gmail.com>
2024-12-31 16:20:25 +0530

    Nav bar

---
+++ src/Components/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const HeroSection = () => {
+  return (
+    <div className='flex flex-col items-center mt-6 lg:mt-20'>
+      <h1 className='text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide'>
+        Virtual build tools for developers.
+      </h1>
+    </div>
+  )
+}
+
